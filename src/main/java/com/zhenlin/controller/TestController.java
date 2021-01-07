29cbd38386cf86f2c84374567745d5bfe394809c
zhangxiangrong <zhangxiangtong@qq.com>
2021-01-07 21:16:42 +0800

    第一次测试提交

---
+++ src/main/java/com/zhenlin/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.zhenlin.controller;
+
+/**
+ * @author caomengxi
+ */
+public class TestController {
+    public static void main(String[] args) {
+        System.out.println("测试一下");
+        System.out.println("没有被管理，就可以随便改");
+
+    }
+}
