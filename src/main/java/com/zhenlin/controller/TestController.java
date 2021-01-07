29d7fd373cd626e6cc334eca063e50c5da4c9d30
zhangxiangrong <zhangxiangtong@qq.com>
2021-01-07 21:19:05 +0800

    第二次修改后测试提交

--- src/main/java/com/zhenlin/controller/TestController.java
+++ src/main/java/com/zhenlin/controller/TestController.java
@@ -7,6 +7,6 @@ public class TestController {
     public static void main(String[] args) {
         System.out.println("测试一下");
         System.out.println("没有被管理，就可以随便改");
-
+        System.out.println("提交到本地库中后，就能显示修改的文件了");
     }
 }
